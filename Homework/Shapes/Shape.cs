32ddd55d10bedcec2545349df3f143fdf1eaf456
Adam Patridge <patridge@gmail.com>
2017-09-21 11:13:07 -0600

    Add files missed by migration.

---
+++ Homework/Shapes/Shape.cs
@@ -0,0 +1,16 @@
+﻿namespace Shapes
+{
+    public abstract class Shape
+    {
+        public double Length { get; protected set; }
+        public double Width { get; protected set; }
+
+        public abstract double GetArea();
+        public abstract string DescribeSelf();
+
+        public override string ToString()
+        {
+            return DescribeSelf();
+        }
+    }
+}
